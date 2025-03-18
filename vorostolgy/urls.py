7f95ead27a5b0c6e813d37d69a4cc1c8fb28ad88
dbejo <bejodavid123@gmail.com>
2025-03-18 16:35:43 +0100

    UI and routing stuff

--- vorostolgy/urls.py
+++ vorostolgy/urls.py
@@ -17,8 +17,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from vorostolgy import views
+
 urlpatterns = [
-    path('menu/', include('menu.urls')),
+    path("", views.home, name="home"),
+    path("elerhetoseg/", views.contact, name="contact"),
+    path('etlap/', include('menu.urls')),
     path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
